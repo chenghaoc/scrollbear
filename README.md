43282a6b7c25b9af905871b6ad8943e401649867
changbenny <changbenny0401@gmail.com>
2016-04-11 22:29:49 +0800

    update structure, make height calculation more accurate

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Scrollbear
-Scrollbear is a tool that maintains scroll position when images loaded. Scrollbear is suitable in blog platforms where exist many photos and images.
+Scrollbear is a tool that maintains scroll position when images loaded. Scrollbear is suitable in blog platforms where exist many photos and images. When images loaded, the scroll position will *jump* and cause a unfriendly experience. Scrollbear will trace the container height and maintain the scroll position when the images loaded and container height change.
 
 #### Installation
 
--- index.js
+++ index.js
@@ -1,59 +1,52 @@
-var container = document.querySelector('#container')
-var scroll = container.parentNode
-var unloadImages = Array.from(container.querySelectorAll('img'))
-// unloadImages.forEach(img => console.dir(img.offsetHeight))
-// Array.from(container.querySelectorAll('img')).forEach(img => {
-//   img.onload = e => {
-//     console.log(e.target)
-//     console.log(scroll.scrollTop)
-//     console.log(document.scrollTop)
-//     console.log(window.scrollTop)
-//     console.log(document.body.scrollTop)
-//     // console.log($(window).scrollTop())
-//     console.log(window.pageYOffset);
-//   }
-// })
-var oldHeight
-var scroll
-requestAnimationFrame(function frame(time) {
-  // console.log(window.pageYOffset);
-  // console.log(container.offsetHeight);
-  var newHeight = container.offsetHeight
-  scroll = getScroll()
-  // container height change, means there's a image loaded
-  if (isHeightChange(oldHeight, newHeight) &&
-    // get loaded image, then determine if it's above the viewport 
-    getLoadedImages(unloadImages)[0].offsetTop < window.scrollY) {
-    console.log('ret');
-    unloadImages = removeLoadedImages(unloadImages)
-    var offset = newHeight - oldHeight
-    returnScroll(window, scroll + offset)
+var scrollbear = (function() {
+  
+  function start(target) {
+    var container = document.querySelector(target)
+    var scroller = container.parentNode
+    var unloadImages = Array.from(container.querySelectorAll('div'))
+    unloadImages.forEach(img => img.caculatedHeight = 0)
+
+    var oldHeight = container.offsetHeight
+    var scroll
+    requestAnimationFrame(function frame(time) {
+      var newHeight = container.offsetHeight
+      scroll = getScroll()
+      // container height change, means there's a image loaded
+      if (isHeightChange(oldHeight, newHeight) &&
+        // get loaded image, then determine if it's above the viewport 
+        getLoadedImages(unloadImages)[0].offsetTop < window.scrollY) {
+        unloadImages = markLoadedImages(unloadImages)
+        var offset = newHeight - oldHeight
+        returnScroll(window, scroll + offset)
+      }
+      oldHeight = newHeight
+      requestAnimationFrame(frame)
+    })
   }
-  oldHeight = newHeight
-  requestAnimationFrame(frame)
-})
 
-function isHeightChange(oldHeight, newHeight) {
-  return oldHeight !== newHeight
-}
-// TODO: take a look of jQuery
-function getScroll() {
-  return window.scrollY
-}
-function returnScroll(target, pos) {
-  target.scrollTo(0, pos)
-}
+  function isHeightChange(oldHeight, newHeight) {
+    return oldHeight !== newHeight
+  }
+  // TODO: take a look of jQuery
+  function getScroll() {
+    return window.scrollY
+  }
+  function returnScroll(target, pos) {
+    target.scrollTo(0, pos)
+  }
+  function markLoadedImages(images) {
+    return images.map(img => {
+      img.caculatedHeight = img.offsetHeight
+      return img
+    })
+  }
+  function getLoadedImages(images) {
+    return images.filter(img => img.offsetHeight > img.caculatedHeight)
+  }
 
-function getLoadedImages(images) {
-  return images.filter(img => {
-    return img.offsetHeight > 0
-  })
-}
-function removeLoadedImages(images) {
-  return images.filter(img => {
-    return img.offsetHeight === 0
-  })
-}
-function isImageAboveViewport() {
+  // Public APIs
+  return {
+    start: start
+  }
+})()
 
-}
