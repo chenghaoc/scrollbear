75f91790a060e267400ce4cfacb6887784958588
changbenny <changbenny0401@gmail.com>
2016-04-13 23:01:46 +0800

    add build process add demo

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var rollup = require('gulp-rollup');
+var babel = require('gulp-babel');
+var rename = require('gulp-rename');
+var util = require('gulp-util');
+var jscs = require('gulp-jscs');
+var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
+var print = require('gulp-print');
+var nodeResolve = require('rollup-plugin-node-resolve');
+var commonjs    = require('rollup-plugin-commonjs');
+
+gulp.task('compile', () => {
+    watch('./src/*.js', compile);
+    return compile();
+});
+
+function compile() {
+    return gulp.src('./src/index.js')
+        .pipe(print(function(filepath) {
+            return 'built: ' + filepath;
+        }))
+        .pipe(plumber({
+            errorHandler: function(err) {
+                console.log(err.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(babel())
+        .pipe(rollup({
+            sourceMap: true,
+            plugins: [ 
+                nodeResolve({ jsnext: true, main: true, browser: true }),
+                commonjs()
+            ]
+        }))
+        .on('error', util.log)
+        .pipe(rename('scrollbear.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./build'));
+}
+
+gulp.task('default', ['compile']);
+gulp.task('lint', function() {
+    return gulp.src('src/**')
+          .pipe(jscs({fix: true, configPath: '.jscsrc'}))
+          .pipe(gulp.dest('src/'));
+});
--- src/index.js
+++ src/index.js
@@ -1,36 +1,40 @@
 var Scrollbear = (function(window, document) {
   function start(
-    target = document.body, 
+    target = document.body,
     changedItem = target.querySelectorAll('img')) {
     var scroller = target
-    var contents = Array.from(scroller.childNodes)
-    var unloadItems = Array.from(changedItem || []).map(img => { return { dom: img, caculatedHeight: 0 } })
-    var oldHeight = contents.reduce((total, content) => total + (content.offsetHeight || 0), 0)
+    var unloadItems = Array.from(changedItem || []).map(img => { return {dom: img, caculatedHeight: 0} })
+    var oldHeight = Array.from(scroller.childNodes).reduce(accumulateHeight, 0)
 
     // use closure to share the scope
     var frame = function() {
-      var newHeight = container.offsetHeight
+      var newHeight = Array.from(scroller.childNodes).reduce(accumulateHeight, 0)
       // save the normal scroll position
       var scroll = getScroll(scroller)
       // container height change, means there's a image loaded
-      if (isHeightChange(oldHeight, newHeight) &&
-        // get loaded image, then determine if it's above the viewport 
-        getLoadedItems(unloadItems)[0].dom.offsetTop < getScroll(scroller)) {
-        // mark that part of item height is already be calculated
-        unloadItems = markLoadedItems(unloadItems)
-        // return to normal scroll position, avoid the page jump
-        // there's only part we set the value of style, avoid sync layout threashing
-        returnScroll(scroller, scroll + (newHeight - oldHeight))
+      if (isHeightChange(oldHeight, newHeight)) {
+        // get loaded image, then determine if it's above the viewport
+        if (getLoadedItems(unloadItems)
+          .filter(item => item.dom.offsetTop < scroll)
+          .map(markLoadedItems).length > 0) {
+            // mark that part of item height is already be calculated
+            // unloadItems = markLoadedItems(unloadItems)
+            // return to normal scroll position, avoid the page jump
+            // there's only part we set the value of style, avoid sync layout threashing
+            returnScroll(scroller, scroll + (newHeight - oldHeight))
+          }
       }
       oldHeight = newHeight
       window.requestAnimationFrame(frame)
     }
-
     return window.requestAnimationFrame(frame)
   }
   function assign(target, prop, value) {
     target[prop] = value
     return target
+  }
+  function accumulateHeight(total, content) {
+    return total + (content.offsetHeight || 0)
   }
   function isHeightChange(oldHeight, newHeight) {
     return oldHeight !== newHeight
@@ -46,8 +50,8 @@ var Scrollbear = (function(window, document) {
     else
       target.scrollTop = pos
   }
-  function markLoadedItems(items) {
-    return items.map(item => assign(item, 'caculatedHeight', item.dom.offsetHeight))
+  function markLoadedItems(item) {
+    return assign(item, 'caculatedHeight', item.dom.offsetHeight)
   }
   function getLoadedItems(items) {
     return items.filter(item => item.dom.offsetHeight > item.caculatedHeight)
